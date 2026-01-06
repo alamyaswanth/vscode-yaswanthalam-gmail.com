a9134d3796bde5b39504021b1544f34c78e7c1b0
yaswanthalam <yaswanthalam@gmail.com>
2026-01-05 21:10:14 -0800

    test

---
+++ Notes/Concepts/__name__==__main__/example/first.py
@@ -0,0 +1,11 @@
+def yas(a , b):
+    return a - b
+
+print("We are subracting a and b in this function")
+
+if __name__ == "__main__":
+    print("This is executed only when we run this file directlty")
+    result = yas(5 , 85)
+    print(result)
+
+    
---
+++ Notes/Concepts/__name__==__main__/example2/third.py
@@ -0,0 +1,7 @@
+def yas(a , b):
+    return a - b
+
+print("We are subracting a and b in this function")
+print("This is executed only when we run this file directlty")
+result = yas(5 , 85)
+print(result)
