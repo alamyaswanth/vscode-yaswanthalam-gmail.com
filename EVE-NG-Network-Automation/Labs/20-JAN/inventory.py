5e407c61f8883293db8abdbd53a03b7774aaa44d
Yaswanth Alam <yaswanthalam@gmail.com>
2026-01-19 23:12:49 -0800

    20-JAN

---
+++ EVE-NG-Network-Automation/Labs/20-JAN/inventory.py
@@ -0,0 +1,18 @@
+routers = [
+    {
+        "device_type": "cisco_ios",
+        "host": "192.168.54.142",
+        "username": "admin",
+        "password": "admin123",
+        "secret": "admin123",
+        "name": "R1"
+    },
+    {
+        "device_type": "cisco_ios",
+        "host": "192.168.54.143",
+        "username": "admin",
+        "password": "admin123",
+        "secret": "admin123",
+        "name": "R2"
+    }
+]
